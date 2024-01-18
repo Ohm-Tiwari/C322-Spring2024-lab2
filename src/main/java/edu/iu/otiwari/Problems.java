01665603752e327338e96921ccd75eca425436af
Ohm-Tiwari <otiwari24@gmail.com>
2024-01-18 12:49:09 -0500

    Fixed problem 2

--- src/main/java/edu/iu/otiwari/Problems.java
+++ src/main/java/edu/iu/otiwari/Problems.java
@@ -3,36 +3,56 @@ package edu.iu.otiwari;
 import java.util.ArrayList;
 
 public class Problems {
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
+    static  boolean isPrime(int n){
+        if(n<=1) {
             return false;
         }
-        for (int i = 2; i < n; i++) {
-            if ((n % 1) == 0) {
-                return false;
-            }
+        for(int i=2;i<=n/2;i++) {
+            if((n%i)==0)
+                return  false;
         }
         return true;
     }
 
 
+
+
+
     public static int[] primeFactors(int n) {
-        // d is a prime factor of n, if d is less than n, d is prime and d is a divisor of n
-        ArrayList<Integer> factors = new ArrayList<>();
+        int[] factors = new int[20];
+        int factorsCount = 0;
 
         while (n % 2 == 0) {
-            factors.add(2);
+            factors[factorsCount++] = 2;
             n /= 2;
         }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+
+        for (int i = 3; i * i <= n; i += 2) {
             while (n % i == 0) {
-                factors.add(i);
+                factors[factorsCount++] = i;
                 n /= i;
             }
         }
+
         if (n > 2) {
-            factors.add(n);
+            factors[factorsCount++] = n;
         }
-        return factors.stream().mapToInt(Integer::intValue).toArray();
+
+        if (factorsCount < factors.length) {
+            int[] newFactors = new int[factorsCount];
+            System.arraycopy(factors, 0, newFactors, 0, factorsCount);
+            factors = newFactors;
+        }
+
+        for (int i = 0; i < factorsCount - 1; i++) {
+            for (int j = 0; j < factorsCount - i - 1; j++) {
+                if (factors[j] > factors[j + 1]) {
+                    int temp = factors[j];
+                    factors[j] = factors[j + 1];
+                    factors[j + 1] = temp;
+                }
+            }
+        }
+        return factors;
     }
 }
